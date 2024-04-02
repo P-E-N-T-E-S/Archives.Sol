cf0e47b9e86adc2e3ebbc04ec4b2af309322313e
Evaldo Cunha F <evaldocunhaf@gmail.com>
2024-04-01 22:32:27 -0300

    iniciacao do projeto anchor

---
+++ programs/archives-sol/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7bYR2BPEbLE5uR22XsUito5GWeDMJAgnocgU4r88NNMA");
+
+#[program]
+pub mod archives_sol {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
